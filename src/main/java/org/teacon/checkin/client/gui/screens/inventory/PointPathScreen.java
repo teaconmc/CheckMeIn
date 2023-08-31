159afe3f7789a5681d499b232b308fbfc00c0b87
3TUSK <git@tritusk.info>
2023-08-30 23:47:20 -0700

    Wrong title

--- src/main/java/org/teacon/checkin/client/gui/screens/inventory/PointPathScreen.java
+++ src/main/java/org/teacon/checkin/client/gui/screens/inventory/PointPathScreen.java
@@ -55,7 +55,7 @@ public class PointPathScreen extends AbstractCheckPointScreen<PointPathMenu> {
         this.renderBackground(guiGraphics);
 
         int centerX = this.width / 2;
-        guiGraphics.drawCenteredString(this.font, Component.translatable("container.check_in.point_unique"), centerX, 20, TITLE_COLOR);
+        guiGraphics.drawCenteredString(this.font, Component.translatable("container.check_in.point_path"), centerX, 20, TITLE_COLOR);
         guiGraphics.drawString(this.font, Component.translatable("container.check_in.team_id"), centerX - 150, 50, LABEL_COLOR);
         this.teamID.render(guiGraphics, mouseX, mouseY, p_282465_);
         guiGraphics.drawString(this.font, Component.translatable("container.check_in.point_name"), centerX + 4, 50, LABEL_COLOR);
